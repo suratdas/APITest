4aef8e6c6ea6a53497b4247a2a6a1e54746df72a
Surat Das <suratdas@users.noreply.github.com>
2015-12-08 10:31:32 -0800

    Added a regular method besides the Test method

--- src/test/java/com/surat/APITest.java
+++ src/test/java/com/surat/APITest.java
@@ -18,13 +18,13 @@ public class APITest {
 	public APITest(){
 		//Server variables
 		//baseURI ="http://api.openweathermap.org/data/2.5/weather?q=london";
-		baseURI ="https://api.github.com/users/mralexgray/repos";
+		baseURI ="https://api.github.com/users/<username>/repos";
 		//port=8080;
 		//authentication = basic("username", "password");
 	}
 
 	@Test
-	public void test() {
+	public void JunitTest() {
 
 		//Add these 3 jar files from rest-assured website if you don't want to use maven. This project uses maven
 		//1. rest-assured-2.4.1-dist  2. json-path-2.4.1-dist  3. json-schema-validator-2.4.1-dist   
@@ -39,7 +39,7 @@ public class APITest {
 
 		//Assertion comparisons
 		get().then().body("id", hasItem(16160992)); //Will throw an exception if condition not met, no output if its met.
-		get().then().body("owner.login", hasItem("mralexgray")); //Will throw an exception if condition not met, no output if its met.
+		get().then().body("owner.login", hasItem("<username>")); //Will throw an exception if condition not met, no output if its met.
 
 		//Extracting particular data
 		System.out.println(get().path("owner.login"));
@@ -60,6 +60,52 @@ public class APITest {
 		//Asserts count of the elements
 		expect().body("owner.login.size()",equalTo(30)).when().get();
 		
+	}
+	
+	public void RegularMethod() {
+		//Define base url for the endpoint
+	        baseURI ="https://api.github.com/users/<username>/";
+	        //Get the response and put it in variables
+	        Response response = get("repos");
+	        ResponseBody responsebody = response.body();
+	        //Get the status code
+	        System.out.println("Status code:"+response.getStatusCode() + " Status line: "+response.statusLine()); //Prings => Status code:200 Status line: HTTP/1.1 200 OK
+	        //Get the response headers
+	        System.out.println("Headers:");
+	        List<Header> headers = response.getHeaders().asList();
+	        for(Header temp : headers) {
+	            System.out.println("\t"+temp.getName() +":"+temp.getValue());
+	        }
+	        //Get the response cookies
+	        Response response1 = get("http://google.com");
+	        System.out.println("Cookie size : "+ response1.cookies().size());
+	        Map<String,String> cookies = response1.getCookies();
+	        Set<String> keys = cookies.keySet();
+	        for(String key:keys){
+	            System.out.println("\t"+key +"=>"+ cookies.get(key));
+	        }
+	        //Process body content : display whole body, array of nodes/single node or assert if it contains some value etc.
+	        System.out.println("Body:\n\t"+responsebody.asString()); //Prints whle body
+	        System.out.println("Example array from response: "+responsebody.path("id")); //prints array of id in the response
+	        System.out.println("Calculate Size: "+responsebody.path("owner.login.size()")); //prints count of an array in response body
+	        try {
+	            response.then().body("id", hasItem(36698447)); //Will throw an exception if condition not met, no output if its met.
+	            System.out.println("Found the item.");
+	        }
+	        catch(AssertionError e){
+	            //Do whatever you want to do to when assertion fails
+	            System.out.println("Exception seen:");
+	            System.out.print("\t"+e.getMessage());
+	        }
+	        //Extracting response another way using JsonPath
+	        JsonPath jsonPath = new JsonPath(responsebody.asString());
+	        List<Integer> winnerIds = jsonPath.get("id");
+	        System.out.println("Using JsonPath:");
+	        System.out.print("\t");
+	        for(Integer temp : winnerIds){
+	            System.out.print(temp+", ");
+	        }
+	        //*/	
 	}
 
 }
